804773e6758e7c16b57bd01c8264160d7e746a4f
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2026-08-19 10:41:19 +0200

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        ssr: {
-            noExternal: ['webcoreui']
-        }
-    },
-    site: 'https://jessica-albers.de/',
-    // base: '/jessica-albers.de/',
-})
+  vite: {
+      ssr: {
+          noExternal: ['webcoreui']
+      }
+  },
 
+  // base: '/jessica-albers.de/',
+  site: 'https://jessica-albers.de/',
+
+  integrations: [svelte()],
+})
